e10e76174ea66557169b8aac4f7a5dff0dd65979
Vladimir Krasnov <435335vova@gmail.com>
2023-06-06 22:35:28 +0300

    Навигация на экран эпизодов

--- app/src/main/java/ru/vladimir/south_park/MainActivity.kt
+++ app/src/main/java/ru/vladimir/south_park/MainActivity.kt
@@ -1,5 +1,21 @@
 package ru.vladimir.south_park
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val bottomNavbar = findViewById<BottomNavigationView>(R.id.main_bnv)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavbar.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/ru/vladimir/south_park/presentation/episodes/EpisodesFragment.kt
@@ -0,0 +1,9 @@
+package ru.vladimir.south_park.presentation.episodes
+
+import androidx.fragment.app.Fragment
+import ru.vladimir.south_park.R
+
+class EpisodesFragment : Fragment(R.layout.fragment_episodes) {
+
+
+}
